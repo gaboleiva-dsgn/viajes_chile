2ec15bbbfc35312aa666c93f0e239a5fa306f89f
Gabriel Leiva <g.leivach@gmail.com>
2024-02-04 05:25:05 -0300

    Se incluyen funciones tooltip, popover, alert y clases css

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,17 @@ $("#btn_submit").click(function (e){
     e.preventDefault();
     alert("Su mensaje fue enviado satisfactoriamente")
 });
+
+// Habilitamos tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    // Habilitamos popover
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const popover = new bootstrap.Popover('.btn_popover', {
+    container: '.box_popover'
+  })
