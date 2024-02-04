efd0668bc3d13df5251796c54557c27c1edd0fb2
Gabriel Leiva <g.leivach@gmail.com>
2024-02-04 05:29:53 -0300

    Se organiza, da formato y documenta el codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,41 @@
+// Habilitamos Smooth scrolling en la clase nav-link del navbar
 $(".nav-link").click(function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-    if (this.hash !== "") {
+    var hash = this.hash;
 
-        event.preventDefault();
-
-        var hash = this.hash;
-
-        $("html, body").animate({
-            scrollTop: $(hash).offset().top
-        }, 1000); 
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      1000
+    );
+  }
 });
 
-$("#btn_submit").click(function (e){
-    e.preventDefault();
-    alert("Su mensaje fue enviado satisfactoriamente")
+// Habilitamos función alert en el boton submit
+$("#btn_submit").click(function (e) {
+  e.preventDefault();
+  alert("Su mensaje fue enviado satisfactoriamente");
 });
 
 // Habilitamos tooltip
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
 
-    // Habilitamos popover
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+// Habilitamos popover
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
 
-const popover = new bootstrap.Popover('.btn_popover', {
-    container: '.box_popover'
-  })
+const popover = new bootstrap.Popover(".btn_popover", {
+  container: ".box_popover",
+});
